20019591c7e13070796234100c22a80b0aeebf18
shabat <vitalii.shabat@gmail.com>
2018-10-09 18:43:53 +0300

    Added 4 students to the existing 3. Added student's knowledge

--- src/main/Application.java
+++ src/main/Application.java
@@ -6,10 +6,16 @@ import person.Student;
 
 public class Application {
     public static void main(String[] args) {
+        ArrayList<Student> studentsList = new ArrayList<Student>();
+        studentsList.add(new Student("Andrew Kostenko", 59));
+        studentsList.add(new Student("Julia Veselkina", 35));
+        studentsList.add(new Student("Maria Perechrest", 66));
+        studentsList.add(new Student("Peter Petrov", 29));
+        studentsList.add(new Student("Oleg Urbanovich", 96));
+        studentsList.add(new Student("Sam Samsonov", 74));
+        studentsList.add(new Student("John Johnson", 87));
+
         University university = new University("CH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko"));
-        university.addStudent(new Student("Julia Veselkina"));
-        university.addStudent(new Student("Maria Perechrest"));
 
         Internship internship = new Internship("Interlink");
         System.out.println("List of internship's students:");
